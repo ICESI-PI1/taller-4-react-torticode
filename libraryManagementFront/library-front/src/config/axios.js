c189fbcb52aae1cd9476f7873465e83513966aaf
Dan-Ale-Es-A <alexandraarenas1705@gmail.com>
2023-11-02 08:51:35 -0500

    feat:project compile succesfully, add url axios

--- libraryManagementFront/library-front/src/config/axios.js
+++ libraryManagementFront/library-front/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 //Debo agregar la ruta en la que se ejecuta la API-Rest
-const instance = axios.create()
+const instance = axios.create({baseURL:"http://localhost:5174/"})
 
 export default instance
